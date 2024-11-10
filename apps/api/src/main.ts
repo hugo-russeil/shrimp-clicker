55a0e71d4add680e1b21defc02dceea8461f372a
Arnaud Mazurier <57632793+Altaks@users.noreply.github.com>
2024-11-10 14:07:27 +0100

    feat: Added Swagger (back) when in development mode + Lucide Icons React (front)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,16 +7,29 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+
+  // Global application module
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  // Setup global exchange endpoint
+  app.setGlobalPrefix('api');
+
+  // Setup Swagger documentation if we are in dev mode
+  if(process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle("Nuit de l'info 2024 - Void LR - API Documentation")
+      .setVersion('1.0')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, documentFactory);
+    Logger.warn("Swagger has been setup since the app has not been started in production mode", "App")
+  }
+
+  Logger.log("API starting on port 3000", "App")
+  await app.listen(process.env.port ?? 3000);
 }
 
 bootstrap();
