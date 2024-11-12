2c4c27c1c44f505bcd87a484918f20734054aab9
Arnaud Mazurier <57632793+Altaks@users.noreply.github.com>
2024-11-12 20:15:36 +0100

    feat: XSS + DDoS protection with throttler middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,11 +8,13 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
 
   // Global application module
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // Setup global exchange endpoint
   app.setGlobalPrefix('api');
 
@@ -34,6 +36,12 @@ async function bootstrap() {
 
     Logger.warn("Swagger has been setup since the app has not been started in production mode", "App")
   }
+
+  app.use(helmet())
+  Logger.log("HelmetJS HTTP Headers defense middleware has been setup")
+
+  app.set('trust proxy', 'loopback');
+  Logger.warn("Trusting requests from the loopback address")
 
   Logger.log("API starting on port 3000", "App")
   await app.listen(process.env.port ?? 3000);
